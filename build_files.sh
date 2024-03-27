0cbf2b3ec8d8f09f1bf174a328fdb6a5c7f211d9
Author <nhussainjee@gmail.com>
2024-03-28 01:41:05 +0500

    ...build file change

--- build_files.sh
+++ build_files.sh
@@ -3,23 +3,3 @@
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
 
-# !/bin/bash
-
-# # Update pip
-# echo "Updating pip..."
-# python3.9 pip install -U pip
-
-# # Install dependencies
-
-# echo "Installing project dependencies..."
-# python3.9 -m pip install -r requirements.txt
-
-# # Make migrations
-# echo "Making migrations..."
-# python3.9 manage.py makemigrations --noinput
-# python3.9 manage.py migrate --noinput2
-# # Collect staticfiles
-# echo "Collect static..."
-# python3.9 manage.py collectstatic --noinput --clear
-
-# echo "Build process completed!"
