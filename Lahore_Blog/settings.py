b614a47fe1cfa8d14954449d1281a4b8f5c1384b
Author <nhussainjee@gmail.com>
2024-03-27 21:44:39 +0500

    ...

--- Lahore_Blog/settings.py
+++ Lahore_Blog/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'Lahore_Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
