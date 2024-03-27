3bb4397eb5e7692d741fe51ba86924bdc802f866
Author <nhussainjee@gmail.com>
2024-03-27 23:24:29 +0500

    ...

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,26 @@
- echo "BUILD START"
- python3.11 -m pip install -r requirements.txt
- python3.11 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#  echo "BUILD START"
+#  python3.11 -m pip install -r requirements.txt
+#  python3.11 manage.py collectstatic --noinput --clear
+#  echo "BUILD END"
+
+#!/bin/bash
+
+# Update pip
+echo "Updating pip..."
+python3.9 pip install -U pip
+
+# Install dependencies
+
+echo "Installing project dependencies..."
+python3.9 -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+# Collect staticfiles
+echo "Collect static..."
+python3.9 manage.py collectstatic --noinput --clear
+
+echo "Build process completed!"
