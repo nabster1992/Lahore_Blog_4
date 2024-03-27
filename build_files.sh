c3cab4a62325386f9212df4341700c3f00e309fa
Author <nhussainjee@gmail.com>
2024-03-27 23:34:50 +0500

    ...

--- build_files.sh
+++ build_files.sh
@@ -7,20 +7,19 @@
 
 # Update pip
 echo "Updating pip..."
-python3.10 pip install -U pip
+python3.12 pip install -U pip
 
 # Install dependencies
 
 echo "Installing project dependencies..."
-python3.10 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 # Make migrations
 echo "Making migrations..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
-
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput2
 # Collect staticfiles
 echo "Collect static..."
-python3.10 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
 
 echo "Build process completed!"
